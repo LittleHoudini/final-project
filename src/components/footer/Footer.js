a136276385610eb267adf8594d5c724aa854dd08
Yaniv Cohen <cardib@Yanivs-MacBook-Air.local>
2022-04-17 20:04:29 +0300

    redesigned footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,27 +3,50 @@
  *****************************************/
 
 import React from 'react';
-
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 import './footer.css'
 /*****************************************
  * * CREATE REACT FUNCTION COMPONENT
  *****************************************/
 
-function getDate() {
 
-  let date = new Date();
-  return date.getFullYear();
-}
-
-// pascal case
 function Footer() {
-  const name = 'Footer';
   return (
-    <footer className="footer">
-      {/* // interpolation */}
-      <p>&copy; {getDate()} {name}</p>
-    </footer>
+    <MDBFooter className="font-small pt-4 mt-4 footer">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">Footer Content</h5>
+            <p>
+              ABOUT
+            </p>
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Links</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!">Link 1</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 2</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 3</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.kissvibe.co.il">KISSVIBE</a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
   );
 }
 
