fed16dc6e7efae53d8e7498f34de6ffbe6229f22
cupcaksen <kseniyavolkov@gmail.com>
2022-04-20 15:56:34 +0300

    data, nav, productsquare css updates

--- src/components/productSquare/ProductSquare.js
+++ src/components/productSquare/ProductSquare.js
@@ -13,15 +13,15 @@
  function ProductSquare(props) {
     const {title,image,text,price} = props.data;
    return (
+    
     <div className={styles.container}>
-      <img className='menu-cover-image' src={image} alt="Original Burger"/>
+   
+    <img className='menu-cover-image' src={image} alt="Original Burger"/>
       <h2 className={styles.h2}>{title}</h2>
       <figure className={styles.figure}>{price}</figure>
       <figure className={styles.figure}>{text}</figure>
-      
       <button className={styles.btn}>הוסף מנה</button>
-    
-    </div>
+      </div>
    );
  }
  export default ProductSquare;
