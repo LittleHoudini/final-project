95c5806bd0f3a6a2ebb0ee461b36a5ce4e8d8b25
Yaniv Cohen <cardib@Yanivs-MacBook-Air.local>
2022-04-23 17:47:15 +0300

    added Card component for 'ProductSquare.js'

--- src/components/productSquare/ProductSquare.js
+++ src/components/productSquare/ProductSquare.js
@@ -3,7 +3,8 @@
  *****************************************/
 
  import React from 'react';
- import styles from './productSquare.module.css';
+import { Card,Button } from 'react-bootstrap';
+import styles from './productSquare.module.css';
  
  /*****************************************
   * * CREATE REACT FUNCTION COMPONENT
@@ -12,18 +13,23 @@
  function ProductSquare(props) {
     const {title,image,text,price} = props.data;
    return (
-    <div className={styles.container}>
-      <img className='menu-cover-image' src={image} alt="Original Burger"/>
-      <h2 className={styles.h2}>{title}</h2>
-
-      <div className={styles.infodiv}> 
-      <figure className={`${styles.price} ${styles.figure}`}>{price}</figure>
-      <figure className={styles.figure}>{text}</figure>
-     </div>
-
-      
-      <button className={styles.btn}>הוסף מנה</button>
-    </div>
+    // <div className={styles.container}>
+    //   <img className='menu-cover-image' src={image} alt="Original Burger"/>
+    //   <h2 className={styles.h2}>{title}</h2>
+    //   <div className={styles.infodiv}> 
+    //   <figure className={`${styles.price} ${styles.figure}`}>{price}</figure>
+    //   <figure className={styles.figure}>{text}</figure>
+    //  </div>
+    //   <button className={styles.btn}>הוסף מנה</button>
+    // </div>
+    <Card>
+      <Card.Img variant="top" src={image}/>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{text}</Card.Text>
+        <Button variant="primary">{price}</Button>
+      </Card.Body>
+    </Card>
    );
  }
  export default ProductSquare;
